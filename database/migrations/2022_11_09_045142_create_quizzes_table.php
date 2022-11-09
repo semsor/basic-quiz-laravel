39a3b65f21e75e2d7c7ed2d548547048331c5e29
Semyon Sorokin <semsor.ru@gmail.com>
2022-11-09 12:02:06 +0700

    Quiz model & controller

--- database/migrations/2022_11_09_045142_create_quizzes_table.php
+++ database/migrations/2022_11_09_045142_create_quizzes_table.php
@@ -15,7 +15,10 @@ return new class extends Migration
     {
         Schema::create('quizzes', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->text('description')->nullable();
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
