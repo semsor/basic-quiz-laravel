de617a8f78e1e6c52867092278a5acd5ec1aea49
Semyon Sorokin <semsor.ru@gmail.com>
2022-11-27 14:22:45 +0700

    Create quiz form

--- resources/views/quiz/index.blade.php
+++ resources/views/quiz/index.blade.php
@@ -1,8 +1,12 @@
 <x-app-layout>
 
-<a href="{{ url('/quiz-create') }}" class="text-sm text-gray-700 underline">Create quiz</a>
+    <a href="{{ url('/quiz-create') }}" class="text-sm text-gray-700 underline">Create quiz</a>
 
-{{$quizzes}}
+    <ul>
+        @foreach ($quizzes as $quiz)
+            <li>{{ $quiz}}</li>
+        @endforeach
+    </ul>
 
 </x-app-layout>
 
